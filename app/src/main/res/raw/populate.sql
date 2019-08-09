c6ca7e9078e131244feb69b898b823281d181792
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-08-09 17:03:05 +0300

    Add new contexts and habits to populate.sql

--- app/src/main/res/raw/populate.sql
+++ app/src/main/res/raw/populate.sql
@@ -10,12 +10,14 @@ insert into HabitEntity(id, name, scheduleId, targetValue) values (12, 'Пишу
 insert into HabitEntity(id, name, scheduleId, targetValue) values (15, 'Ем 2 ладошки', 1, 3);
 insert into HabitEntity(id, name, scheduleId, targetValue) values (13, 'Кардио-тренировка на голодный желудок', 2, 5);
 insert into HabitEntity(id, name, scheduleId, targetValue) values (14, 'Не ем 1 час после тренировки (силовой или кардио)', 2, 5);
+insert into HabitEntity(id, name, scheduleId, targetValue) values (16, 'Сделал вакуум 3 раза по 10 сек', 1, 5);
 insert into ContextHabitJoin(contextId, habitId) values (1,10);
 insert into ContextHabitJoin(contextId, habitId) values (1,11);
 insert into ContextHabitJoin(contextId, habitId) values (1,12);
 insert into ContextHabitJoin(contextId, habitId) values (1,13);
 insert into ContextHabitJoin(contextId, habitId) values (1,14);
 insert into ContextHabitJoin(contextId, habitId) values (1,15);
+insert into ContextHabitJoin(contextId, habitId) values (1,16);
 
 -- Context 2
 insert into ContextEntity(id, name, parentContextId) values (2, 'еда начало', 0);
@@ -77,9 +79,19 @@ insert into ContextHabitJoin(contextId, habitId) values (9,90);
 -- Context 10
 insert into ContextEntity(id, name, parentContextId) values (10, 'еда после', 0);
 insert into HabitEntity(id, name, scheduleId, targetValue) values (100, 'Ответил на вопросы YouAte', 1, 3);
+insert into HabitEntity(id, name, scheduleId, targetValue) values (101, 'Принял милдронат 500 гр', 1, 1);
 insert into ContextHabitJoin(contextId, habitId) values (10,100);
+insert into ContextHabitJoin(contextId, habitId) values (10,101);
 
 -- Context 11
 insert into ContextEntity(id, name, parentContextId) values (11, 'кафе выбираю еду', 0);
 insert into HabitEntity(id, name, scheduleId, targetValue) values (110, 'Заказал только напиток в кафе', 2, 3);
 insert into ContextHabitJoin(contextId, habitId) values (11,110);
+
+-- Context 12
+insert into ContextEntity(id, name, parentContextId) values (12, 'кафе ухожу', 0);
+insert into ContextHabitJoin(contextId, habitId) values (12,40);
+
+-- Context 13
+insert into ContextEntity(id, name, parentContextId) values (13, 'магазин', 0);
+insert into ContextHabitJoin(contextId, habitId) values (13,20);
