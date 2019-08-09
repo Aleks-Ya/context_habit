5bba8da9178303426a7f34ccb0c878ed08f9850a
Aleksei Iablokov <aleksei_iablokov@epam.com>
2019-08-09 22:47:35 +0300

    Fix reading populate.sql

--- app/src/main/java/ru/yaal/contexthabit/ContextHabitApp.java
+++ app/src/main/java/ru/yaal/contexthabit/ContextHabitApp.java
@@ -1,12 +1,47 @@
 package ru.yaal.contexthabit;
 
 import android.app.Application;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import ru.yaal.contexthabit.android.R;
 import ru.yaal.contexthabit.service.Singleton;
 
 public class ContextHabitApp extends Application {
+
     public void onCreate() {
         super.onCreate();
-        Singleton.init(getApplicationContext());
+        Singleton.init(getApplicationContext(), new PopulateDatabaseCallback());
+    }
+
+    private class PopulateDatabaseCallback extends RoomDatabase.Callback {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            Log.i("Database", "Populating database");
+            try {
+                InputStream is = getResources().openRawResource(R.raw.populate);
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader buffer = new BufferedReader(isr);
+                String line;
+                while ((line = buffer.readLine()) != null) {
+                    if (line.isEmpty() || line.startsWith("--")) {
+                        continue;
+                    }
+                    db.execSQL(line);
+                }
+                buffer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
     }
 }
--- app/src/main/java/ru/yaal/contexthabit/service/Singleton.java
+++ app/src/main/java/ru/yaal/contexthabit/service/Singleton.java
@@ -1,23 +1,14 @@
 package ru.yaal.contexthabit.service;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
-import ru.yaal.contexthabit.android.R;
 import ru.yaal.contexthabit.repo.Repository;
 import ru.yaal.contexthabit.repo.RepositoryImpl;
 import ru.yaal.contexthabit.repo.room.AppDatabase;
@@ -26,11 +17,11 @@ public final class Singleton {
     public static Repository repository;
     public static RenewService renewService;
 
-    public static void init(Context context) {
+    public static void init(Context context, RoomDatabase.Callback populateDatabaseCallback) {
         AppDatabase database = Room.databaseBuilder(
                 context, AppDatabase.class, "context_habit")
                 .allowMainThreadQueries()
-                .addCallback(new PopulateDatabaseCallback())
+                .addCallback(populateDatabaseCallback)
                 .build();
 
         repository = new RepositoryImpl(database.contextDao(), database.habitDao(),
@@ -41,29 +32,6 @@ public final class Singleton {
 
         WorkManager workManager = WorkManager.getInstance(context);
         runHabitRenewWorker(workManager);
-    }
-
-    private static class PopulateDatabaseCallback extends RoomDatabase.Callback {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-            Log.i("Database", "Populating database");
-            try {
-                InputStream is = Resources.getSystem().openRawResource(R.raw.populate);
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader buffer = new BufferedReader(isr);
-                String line;
-                while ((line = buffer.readLine()) != null) {
-                    if (line.isEmpty() || line.startsWith("--")) {
-                        continue;
-                    }
-                    db.execSQL(line);
-                }
-                buffer.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e.getMessage(), e);
-            }
-        }
     }
 
     private static void runHabitRenewWorker(WorkManager workManager) {
